50d11670496a26e26f10493e742fb5d53133c4d0
Izora9099 <ndifonlemuel@gmail.com>
2023-06-16 15:26:09 +0100

    comit

--- uploadData.js
+++ uploadData.js
@@ -2,6 +2,7 @@
 use('ReactFrontendProject')
 //need to modify the code to match my hardcoded stuff, but i no know how i'ma do it
 // Insert a few documents into the PortfolioData collection.
+/*
 db.getCollection('projects').insertMany([
   {
     image:
@@ -59,74 +60,32 @@ db.getCollection('projects').insertMany([
    
     content: 'Transform your space with captivating digital art'
    
-  },
-  {
-    image:
-      'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a1835363-197c-4d53-80da-40fe9251e895/dfwr6m9-3d2a3139-bea6-4bc4-a8ae-266f4b9f79af.jpg/v1/fill/w_1000,h_800,q_70,strp/digital_portrait_of_beautiful_lady_by_izoraquinn_dfwr6m9-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTAyNCIsInBhdGgiOiJcL2ZcL2ExODM1MzYzLTE5N2MtNGQ1My04MGRhLTQwZmU5MjUxZTg5NVwvZGZ3cjZtOS0zZDJhMzEzOS1iZWE2LTRiYzQtYThhZS0yNjZmNGI5Zjc5YWYuanBnIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.s7NMXKtucFj41KgT-JTzhJ2mSwa2_Ide23ZsOfgF5qk',
-      seemore: 'https://www.pinterest.com/izoragraphics/',
-      title: 'Digital Art',
-   
-    content: 'Transform your space with captivating digital art'
-   
-  },
-  {
-    image:
-      'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a1835363-197c-4d53-80da-40fe9251e895/dfxafqx-4d9f86c9-4aa1-4af1-a0a1-3684140d77ad.png/v1/fill/w_1280,h_1920,q_80,strp/digital_art_of_beautiful_african_female_by_izoraquinn_dfxafqx-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2ExODM1MzYzLTE5N2MtNGQ1My04MGRhLTQwZmU5MjUxZTg5NVwvZGZ4YWZxeC00ZDlmODZjOS00YWExLTRhZjEtYTBhMS0zNjg0MTQwZDc3YWQucG5nIiwiaGVpZ2h0IjoiPD0xOTIwIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uud2F0ZXJtYXJrIl0sIndtayI6eyJwYXRoIjoiXC93bVwvYTE4MzUzNjMtMTk3Yy00ZDUzLTgwZGEtNDBmZTkyNTFlODk1XC9pem9yYXF1aW5uLTQucG5nIiwib3BhY2l0eSI6OTUsInByb3BvcnRpb25zIjowLjQ1LCJncmF2aXR5IjoiY2VudGVyIn19.CguSTDJb0UeFivh0bwAEEqWpEvldNYoMSWYZFCfe1gM',
-   seemore: 'https://www.pinterest.com/izoragraphics/', title: 'Digital Art',
-    
-    content: 'Transform your space with captivating digital art'
-   
-  },
-  {
-    image:
-      'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a1835363-197c-4d53-80da-40fe9251e895/dfxzhfg-607cafec-ce23-4570-995c-085b8d751eb3.png/v1/fill/w_1280,h_1600,q_80,strp/legit_african_beauty_by_izoraquinn_dfxzhfg-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2ExODM1MzYzLTE5N2MtNGQ1My04MGRhLTQwZmU5MjUxZTg5NVwvZGZ4emhmZy02MDdjYWZlYy1jZTIzLTQ1NzAtOTk1Yy0wODViOGQ3NTFlYjMucG5nIiwiaGVpZ2h0IjoiPD0xNjAwIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uud2F0ZXJtYXJrIl0sIndtayI6eyJwYXRoIjoiXC93bVwvYTE4MzUzNjMtMTk3Yy00ZDUzLTgwZGEtNDBmZTkyNTFlODk1XC9pem9yYXF1aW5uLTQucG5nIiwib3BhY2l0eSI6OTUsInByb3BvcnRpb25zIjowLjQ1LCJncmF2aXR5IjoiY2VudGVyIn19.x-yXL11WtyPH5cBz6-JcUVQEdp5v4RJd_VbjUqYsFnU',
-   seemore: 'https://www.pinterest.com/izoragraphics/', title: 'Digital Art',
-    
-    content: 'Transform your space with captivating digital art'
-   
-  },
-  {
-    image:
-      'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a1835363-197c-4d53-80da-40fe9251e895/dfx6c94-547dd5a7-5c4a-44ca-8433-1bd37d90a516.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2ExODM1MzYzLTE5N2MtNGQ1My04MGRhLTQwZmU5MjUxZTg5NVwvZGZ4NmM5NC01NDdkZDVhNy01YzRhLTQ0Y2EtODQzMy0xYmQzN2Q5MGE1MTYuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.9IBBMClBv8yd6x4Pi7KXkHlrpzsXC7KINnbvCXfgO6k',
-    seemore: 'https://www.pinterest.com/izoragraphics/', title: 'Digital Art',
-   
-    content: 'Transform your space with captivating digital art'
-   
-  },
-  {
-    image:
-      'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a1835363-197c-4d53-80da-40fe9251e895/dfwtyj3-33d83b2c-10e5-4695-b871-f27d01ad068d.jpg/v1/fill/w_1000,h_800,q_70,strp/gorgeous_black_cinderella_digital_art__by_izoraquinn_dfwtyj3-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTAyNCIsInBhdGgiOiJcL2ZcL2ExODM1MzYzLTE5N2MtNGQ1My04MGRhLTQwZmU5MjUxZTg5NVwvZGZ3dHlqMy0zM2Q4M2IyYy0xMGU1LTQ2OTUtYjg3MS1mMjdkMDFhZDA2OGQuanBnIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.Qh0iGTxy7D_PgOfekhgBvjjmB8x443mrUJeCATpAJBc',
-  seemore: 'https://www.pinterest.com/izoragraphics/',   title: 'Digital Art',
-   
-    content: 'Transform your space with captivating digital art'
-   
   }
 ])
-
+*/
 // Insert a few documents into the PortfolioData collection.
-// db.getCollection('testimonials').insertMany(
-//  [
-//         {
-//           avatar: 'https://cdn.fstoppers.com/styles/large-16-9/s3/lead/2019/01/tips-for-professional-portraits.jpg',
-//           name: 'James Brown',
-//           review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus velit omnis deserunt corrupti perferendis quisquam officia cupiditate ut impedit magnam iusto numquam odio eos, itaque, explicabo ipsum esse dolorem unde!"
-//         },
+ db.getCollection('testimonials').insertMany(
+ [
+         {
+           avatar: 'https://cdn.fstoppers.com/styles/large-16-9/s3/lead/2019/01/tips-for-professional-portraits.jpg',
+           name: 'James Brown',
+           review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus velit omnis deserunt corrupti perferendis quisquam officia cupiditate ut impedit magnam iusto numquam odio eos, itaque, explicabo ipsum esse dolorem unde!"
+         },
 
-//         {
-//           avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3rvvFzP2v-40awpf_uw6dq1UVh8R3jkGM2rrrqIbd6Ih3_d_fCEW8Fn0sl5OsJ1qaLuo&usqp=CAU',
-//           name: 'Tina Snow ',
-//           review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus velit omnis deserunt corrupti perferendis quisquam officia cupiditate ut impedit magnam iusto numquam odio eos, itaque, explicabo ipsum esse dolorem unde!"
-//         },
-//         {
-//           avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJpfFkpLYgdR4R0-ZomZ0ixnZVgLQUOnZnWQ&usqp=CAU',
-//           name: 'Kevin Roussel',
-//           review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus velit omnis deserunt corrupti perferendis quisquam officia cupiditate ut impedit magnam iusto numquam odio eos, itaque, explicabo ipsum esse dolorem unde!"
-//         },
-//         {
-//           avatar: 'https://images.squarespace-cdn.com/content/v1/530ce8d1e4b067ea68a9f821/1612484390216-5NVBC0NJJTFP1OPNRU6F/corporate%2Bbusiness%2Bheadshots%2Blos%2Bangeles_Danielle%2BSpires.jpg',
-//           name: 'Louisa McBrown',
-//           review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus velit omnis deserunt corrupti perferendis quisquam officia cupiditate ut impedit magnam iusto numquam odio eos, itaque, explicabo ipsum esse dolorem unde!"
-//         },
-
-//       ]
-// );
+         {
+           avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3rvvFzP2v-40awpf_uw6dq1UVh8R3jkGM2rrrqIbd6Ih3_d_fCEW8Fn0sl5OsJ1qaLuo&usqp=CAU',
+           name: 'Tina Snow ',
+           review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus velit omnis deserunt corrupti perferendis quisquam officia cupiditate ut impedit magnam iusto numquam odio eos, itaque, explicabo ipsum esse dolorem unde!"
+         },
+         {
+           avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJpfFkpLYgdR4R0-ZomZ0ixnZVgLQUOnZnWQ&usqp=CAU',
+           name: 'Kevin Roussel',
+           review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus velit omnis deserunt corrupti perferendis quisquam officia cupiditate ut impedit magnam iusto numquam odio eos, itaque, explicabo ipsum esse dolorem unde!"
+         },
+         {
+           avatar: 'https://images.squarespace-cdn.com/content/v1/530ce8d1e4b067ea68a9f821/1612484390216-5NVBC0NJJTFP1OPNRU6F/corporate%2Bbusiness%2Bheadshots%2Blos%2Bangeles_Danielle%2BSpires.jpg',
+           name: 'Louisa McBrown',
+           review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus velit omnis deserunt corrupti perferendis quisquam officia cupiditate ut impedit magnam iusto numquam odio eos, itaque, explicabo ipsum esse dolorem unde!"
+         },
+       ]
+ );
