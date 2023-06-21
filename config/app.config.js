e380fe5d4159ead96e675c80b72fba11a3717936
Izora9099 <ndifonlemuel@gmail.com>
2023-06-21 16:58:27 +0100

    commit

--- config/app.config.js
+++ config/app.config.js
@@ -1,7 +1,7 @@
 require('dotenv').config;
 
 const MONGO_DB_CONFIG = {
-    DB: 'mongodb+srv://izoragraphics:10ccgkePk8yaCZU7@reactbackendportfolio.btn8brr.mongodb.net/?retryWrites=true&w=majority'
+    DB: 'mongodb+srv://izoragraphics:10ccgkePk8yaCZU7@reactbackendportfolio.btn8brr.mongodb.net/ReactBackendProject?retryWrites=true&w=majority'
 }
 module.exports = {
     MONGO_DB_CONFIG
